6edfbd6b1f3cab8f20b58a5d523db47bc04336b4
Leonardo12-gif <frezza.trafego@gmail.com>
2025-05-13 07:32:50 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,8 @@ const nextConfig = {
   // Set basePath and assetPrefix if your site is served from a sub-path on GitHub Pages
   // e.g., https://username.github.io/repository-name/
   // The repository name is "frezza-digital-growth-hub"
-  basePath: "/frezza-digital-growth-hub",
-  assetPrefix: "/frezza-digital-growth-hub/",
+  basePath: "/frezza_digitalnextjs_site",
+  assetPrefix: "//frezza_digitalnextjs_site/",
   // trailingSlash: true, // Optional: if you prefer URLs with a trailing slash
 };
 
